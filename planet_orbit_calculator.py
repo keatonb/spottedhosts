51b5d7df72ee446d419bab11d008cfb3533214c4
Saki Kamon <SKGemini@users.noreply.github.com>
2015-01-29 17:26:48 -0600

    Update planet_orbit_calculator.py

--- planet_orbit_calculator.py
+++ planet_orbit_calculator.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from math import pi
+import math
 
 # Constants
 G = 6.67259e-8 #Gravitational constant (cm^2 g^-1 s^-2)
@@ -9,7 +9,7 @@ Msun = 1.99e33 #Solar mass (g)
 Mearth = 5.976e27 #Earth mass (g)
 Yearinsecs = 365.*86400. #length of Earth year (s)
 
-def stellarreflextime(M,N,P, i=True):
+def stellarreflextime(M,N,P,i=0):
     """Returns stellar reflex time in seconds
 
     inputs:
@@ -18,7 +18,7 @@ def stellarreflextime(M,N,P, i=True):
     P = Orbital period in years
     i = Inclination in radians
     """
-
+    
     # Convert to CGS units
     M = M * Msun
     N = N * Mearth
@@ -28,7 +28,7 @@ def stellarreflextime(M,N,P, i=True):
     T = M + N
     
     # Using the input values, we find the semimajor axis given by Kepler's 3rd Law.
-    a = (((P**2)*G*T)/(4*(pi**2)))**(1./3)
+    a = (((P**2)*G*T)/(4*(math.pi**2)))**(1./3)
     
     # Consider the relationship M * r = a * N. We find the radius of the star's wobble around the center of mass in centimeters.
     r = a * N / M
@@ -40,29 +40,16 @@ def stellarreflextime(M,N,P, i=True):
     return l
 
 if __name__ == "__main__":
-    # Give value for the stellar mass, planet mass, and orbital period.
+    # Give value for the stellar mass, planet mass, orbital period, and inclination.
     M = int(input("Stellar mass in Solar masses:"))
     N = int(input("Planet mass in Earth masses:"))
     P = int(input("Orbital period in years:"))
-    l = stellarreflextime(M,N,P, i=True)
+    i = int(input("Inclination in radians:"))
+    # Consider an inclination of a planetary orbit, as it will affect the measured delay time.
+    if i is not 0:
+        M = M * math.sin(i)
+        print M
+    l = stellarreflextime(M,N,P,i=0)
     r = l * c
     print "The radius of the star's wobble around the center of mass is", r, "centimeters or", l, "lightseconds."
-    
-# Consider taking a list of times and each time has been perturbed by an orbital time delay.
-times = []
-for x in times:
-    y = P + sin(x)
-    z = r + sin(x)
-    # Plot the perturbed times with respect to the original times.
-    plt.figure(1)
-    plt.xlabel('Time (sec)')
-    plt.subplot(211)
-    plt.plot(x, y)
-    plt.subplot(212)
-    plt.plot(x, z)
-    plt.subplot(213)
-    plt.plot(x, sin(x))
-    plt.show()
-
-
 
