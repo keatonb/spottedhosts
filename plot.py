8bf5ee9cb08bcaec049d6cf52c2a9adb8ef1cc85
SKGemini <supernova@wireless-10-145-185-245.public.utexas.edu>
2015-03-03 16:18:32 -0600

    removed header

--- plot.py
+++ plot.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 24 12:53:01 2015
-
-@author: supernova
-"""
+#! /usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
 from planet_orbit_calculator import stellarreflextime
