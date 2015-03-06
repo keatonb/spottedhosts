31dec833df0f353f2faa3e0260974f2c7925ac7d
SKGemini <supernova@wireless-10-145-39-66.public.utexas.edu>
2015-03-06 12:59:54 -0600

    added information to try identify what causes the different lc for each generation

--- macula_lc.py
+++ macula_lc.py
@@ -2,9 +2,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from planet_orbit_calculator import timeoffset
-from pymacula import MaculaModel, Spot
+from pymacula import MaculaModel, Spot, Star
+
+#Reference: https://github.com/timothydmorton/pymacula
+
+#Here are the default settings:
+#pymacula.MaculaModel(self, t=None, fobs=None, tmin=0, tmax=500, t_start=None, t_end=None, star=None, 
+#  spots=None, nspots=3, inst_offsets=None, blend_factors=None)
+#Spot(self, lon=None, lat=None, alpha_max=5.0, contrast=0.3, tmax=None, lifetime=None, ingress=None, egress=None)
+#Star(self, incl=1.5707963267948966, Peq=30.0, kappa2=0.3, kappa4=0.3, c1=0.3999, c2=0.4269, c3=-0.0227, c4=-0.839, 
+#   d1=0.3999, d2=0.4269, d3=-0.0227, d4=-0.839)
 
 testspot = Spot(lon=0, lat=0, alpha_max=5.0, contrast=0.3, tmax=1, lifetime=1, ingress=1, egress=1)
+teststar = Star()
+
 
 model = MaculaModel(spots=[testspot]) # default
 ts = np.arange(0,500,0.05)
