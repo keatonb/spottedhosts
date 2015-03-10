3f625a6dfc45141a0a0080b947747d021915f607
SKGemini <supernova@wireless-10-145-19-235.public.utexas.edu>
2015-03-10 14:21:53 -0500

    added labeling for plot

--- macula_lc.py
+++ macula_lc.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from planet_orbit_calculator import timeoffset
-from pymacula import MaculaModel, Spot
+from pymacula import MaculaModel, Spot, Star
 
 #Reference: https://github.com/timothydmorton/pymacula
 
@@ -20,13 +20,17 @@ from pymacula import MaculaModel, Spot
 
 
 testspot = Spot(lon=0, lat=0, alpha_max=5.0, contrast=0.3, tmax=1, lifetime=1, ingress=1, egress=1)
+teststar = Star()
 
-model = MaculaModel(spots=[testspot]) #default, no randomness
+model = MaculaModel(spots=[testspot],star=[teststar]) #default, no randomness
 ts = np.arange(0,500,0.05)
 plt.plot(ts, model(ts))
 
 t = timeoffset(ts,5.,50.,0)
 plt.plot(ts, model(t))
+plt.title("LCs Comparing Time vs Perturbed Time Models")
+plt.xlabel("Time (s)")
+plt.ylabel("Flux")
 plt.show()
 
 print np.sum((model(ts) - model(t))**2) #difference
